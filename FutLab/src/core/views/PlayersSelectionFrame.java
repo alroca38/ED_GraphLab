08233d40cac9e5e28e6b4f0a32d5181e097b8a6e
AlR <alroca38@gmail.com>
2024-10-11 23:44:20 -0500

    New Frame Commit

--- FutLab/src/core/views/PlayersSelectionFrame.java
+++ FutLab/src/core/views/PlayersSelectionFrame.java
@@ -9,10 +9,7 @@ import javax.swing.JOptionPane;
 
 public class PlayersSelectionFrame extends javax.swing.JFrame {
 
-    JFrame menuFrame;
-
-    public PlayersSelectionFrame(JFrame menuFrame) {
-        this.menuFrame = menuFrame;
+    public PlayersSelectionFrame() {
         BackgroundPanel background = new BackgroundPanel("/resources/images/DarkBG.png");
         setContentPane(background);
         setResizable(false);
@@ -30,12 +27,19 @@ public class PlayersSelectionFrame extends javax.swing.JFrame {
         backButton = new javax.swing.JButton();
         jScrollPane2 = new javax.swing.JScrollPane();
         playersList = new javax.swing.JList<>();
+        adjacencySelectionButton = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        confirmationButton.setText("Confirmar");
+        confirmationButton.setText("Empezar");
+        confirmationButton.setEnabled(false);
+        confirmationButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                confirmationButtonActionPerformed(evt);
+            }
+        });
 
-        playersSelectionButton.setText("Seleccionar archivo de jugadores");
+        playersSelectionButton.setText("Ingresar jugadores");
         playersSelectionButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 playersSelectionButtonActionPerformed(evt);
@@ -54,6 +58,13 @@ public class PlayersSelectionFrame extends javax.swing.JFrame {
         playersList.setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION);
         jScrollPane2.setViewportView(playersList);
 
+        adjacencySelectionButton.setText("Ingresar adyacencias");
+        adjacencySelectionButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                adjacencySelectionButtonActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -61,32 +72,37 @@ public class PlayersSelectionFrame extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(80, 80, 80)
+                        .addGap(63, 63, 63)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(backButton)
-                            .addComponent(confirmationButton, javax.swing.GroupLayout.PREFERRED_SIZE, 109, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGap(18, 18, Short.MAX_VALUE))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addContainerGap(35, Short.MAX_VALUE)
-                        .addComponent(playersSelectionButton)
-                        .addGap(28, 28, 28)))
+                            .addComponent(adjacencySelectionButton)
+                            .addComponent(playersSelectionButton, javax.swing.GroupLayout.PREFERRED_SIZE, 139, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(77, 77, 77)
+                        .addComponent(confirmationButton, javax.swing.GroupLayout.PREFERRED_SIZE, 109, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(77, 77, 77)
+                        .addComponent(backButton)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 64, Short.MAX_VALUE)
                 .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(34, 34, 34))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(259, 259, 259)
-                .addComponent(playersSelectionButton)
-                .addGap(18, 18, 18)
-                .addComponent(confirmationButton)
-                .addGap(71, 71, 71)
-                .addComponent(backButton)
-                .addContainerGap(83, Short.MAX_VALUE))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 255, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(31, 31, 31))
+                .addContainerGap(214, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(confirmationButton)
+                        .addGap(18, 18, 18)
+                        .addComponent(playersSelectionButton)
+                        .addGap(18, 18, 18)
+                        .addComponent(adjacencySelectionButton)
+                        .addGap(54, 54, 54)
+                        .addComponent(backButton)
+                        .addGap(60, 60, 60))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 255, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(31, 31, 31))))
         );
 
         pack();
@@ -105,7 +121,6 @@ public class PlayersSelectionFrame extends javax.swing.JFrame {
                 JOptionPane.showMessageDialog(null, response.getMessage(), "Error " + response.getStatus(), JOptionPane.WARNING_MESSAGE);
             } else {
                 JOptionPane.showMessageDialog(null, response.getMessage(), "Response Message", JOptionPane.INFORMATION_MESSAGE);
-
             }
         }
     }//GEN-LAST:event_playersSelectionButtonActionPerformed
@@ -114,13 +129,22 @@ public class PlayersSelectionFrame extends javax.swing.JFrame {
         dispose();
     }//GEN-LAST:event_backButtonActionPerformed
 
+    private void adjacencySelectionButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_adjacencySelectionButtonActionPerformed
+        
+    }//GEN-LAST:event_adjacencySelectionButtonActionPerformed
+
+    private void confirmationButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_confirmationButtonActionPerformed
+        
+    }//GEN-LAST:event_confirmationButtonActionPerformed
+
     @Override
     public void dispose() {
         super.dispose();
-        menuFrame.setVisible(true);
+        MenuFrame.getInstance().setVisible(true);
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton adjacencySelectionButton;
     private javax.swing.JButton backButton;
     private javax.swing.JButton confirmationButton;
     private javax.swing.JScrollPane jScrollPane2;
--- FutLab/src/main/Main.java
+++ FutLab/src/main/Main.java
@@ -23,7 +23,7 @@ public class Main {
         }
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new MenuFrame().setVisible(true);
+                MenuFrame.getInstance().setVisible(true);
             }
         });
     }
